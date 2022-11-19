e866ecb9337783f65d00a912ff44f8abc26f9cde
dan-serraf <55733346+dan-serraf@users.noreply.github.com>
2022-11-19 21:24:31 +0100

    Add files via upload

---
+++ code/get-data/riot_extractor/config.py
@@ -0,0 +1,13 @@
+import os 
+
+
+PRO_CSV_PATH = os.path.join(
+    os.path.join(os.path.dirname(os.getcwd()), "dataset"), 
+    "pro_list_updated.csv"
+)
+
+
+MATCH_CSV_PATH = os.path.join(
+    os.path.join(os.path.dirname(os.getcwd()), "dataset"), 
+    "match.csv"
+)
---
+++ code/get-data/riot_extractor/extractor.py
@@ -0,0 +1,94 @@
+from urllib import response
+import requests
+import json
+from decorator import request
+from soupsieve import match # custom decorator
+from constant import (
+    PUUID_ENDPOINT, 
+    MATCH_ENDPOINT, 
+    MATCH_DETAIL_ENDPOINTS, 
+    MATCH_DETAIL_TIMELINE, 
+    HEADERS, 
+    RIOT_SERVER, 
+    SUMM_ENDPOINT
+    ) # endpoints
+
+
+def get_server(server):
+    """
+        return server, region 
+    """
+    server += "1"
+    if server.lower() not in RIOT_SERVER: 
+        raise ValueError(f"{server} is unkown. Please, select one of the following servers : ", RIOT_SERVER.keys())
+    return RIOT_SERVER[server.lower()]
+
+
+
+class UserDataExtractor: 
+
+    """
+        A class responsible for extracting user related info. 
+    """
+    @request 
+    def get_summoner_info(self, summoner_name, server="EUW1"): 
+        """
+            retrieve user informations based on its summoner's name, and the endpoint 
+        """
+        server, _ = get_server(server)
+        endpoint = server + SUMM_ENDPOINT + summoner_name
+        r = requests.get(endpoint, headers=HEADERS)
+        return {
+            "response" : r, 
+            "endpoint" : endpoint
+        }
+
+
+    @request
+    def get_user_info(self, account_id, server="EUW1"): 
+        """
+            retrieve user informations and the endpoint in order to be consumed
+        """
+        server, _ = get_server(server)
+        endpoint = server + PUUID_ENDPOINT + account_id
+        r = requests.get(endpoint, headers=HEADERS)
+        return {
+            "response" : r, 
+            "endpoint" : endpoint
+        }
+
+
+    @request
+    def get_matches(self, puuid, region="europe"): 
+        """
+            Returns a list of game'id and the endpoint in order to be consumed
+        """
+        _, region = get_server(region)
+        endpoint = region + MATCH_ENDPOINT + f"{puuid}/ids?type=ranked&start=0&count=100"
+        r = requests.get(endpoint, headers=HEADERS)
+        return {
+            "response" : r, 
+            "endpoint" : endpoint
+        }
+
+
+
+class MatchDataExtractor: 
+    """
+        A class responsible for extracting a match informations
+    """
+    
+    @request
+    def retrieve_match_content(self, match_id, region="europe"): 
+        """
+            Returns a game's content and the endpoint in order to be consumed
+        """
+        _, region = get_server(region)
+        endpoint = region + MATCH_DETAIL_ENDPOINTS + match_id
+        r = requests.get(endpoint, headers=HEADERS)
+
+        return {
+            "response" : r, 
+            "endpoint" : endpoint
+        }
+
